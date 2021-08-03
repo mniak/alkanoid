70f55e6f5320ec6dd41933bba8a05c26a628ae7b
Andre Soares <andregsilv@gmail.com>
2021-08-03 19:41:16 -0300

    data

---
+++ data.go
@@ -0,0 +1,39 @@
+package main
+
+import "time"
+
+type Account struct {
+	ID             int
+	DocumentNumber string
+}
+
+type OperationType int
+
+type Transaction struct {
+	ID            int
+	AccountID     int
+	OperationType OperationType
+	Amount        float64
+	EventDate     time.Time
+}
+
+const (
+	CompraAVista    = 1
+	CompraParcelada = 2
+	Saque           = 3
+	Pagamento       = 4
+)
+
+func (ot OperationType) String() string {
+	switch ot {
+	case CompraAVista:
+		return "COMPRA A VISTA"
+	case CompraParcelada:
+		return "COMPRA PARCELADA"
+	case Saque:
+		return "SAQUE"
+	case Pagamento:
+		return "PAGAMENTO"
+	}
+	return "TIPO DE OPERACAO INVALIDO"
+}
