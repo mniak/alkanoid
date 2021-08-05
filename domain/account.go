07af36ad1665e3da04db7d7fb8868c8c7415a987
Andre Soares <andregsilv@gmail.com>
2021-08-05 13:18:23 -0300

    account id

--- domain/account.go
+++ domain/account.go
@@ -5,18 +5,38 @@ import (
 )
 
 type Account struct {
-	ID             int
-	DocumentNumber DocumentNumber
+	id  int
+	doc DocumentNumber
 }
 
 func NewAccount(doc DocumentNumber) Account {
 	return Account{
-		DocumentNumber: doc,
+		doc: doc,
+	}
+}
+
+func ExistingAccount(id int, doc DocumentNumber) Account {
+	return Account{
+		id:  id,
+		doc: doc,
 	}
 }
 
 func (a Account) Validate() error {
 	var err error
-	err = multierror.Append(err, a.DocumentNumber.Validate())
+	err = multierror.Append(err, a.doc.Validate())
 	return err
+}
+
+func (a Account) ID() int {
+	return a.id
+}
+
+func (a Account) DocumentNumber() DocumentNumber {
+	return a.doc
+}
+
+func (a Account) WithID(id int) Account {
+	a.id = id
+	return a
 }
