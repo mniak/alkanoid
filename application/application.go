c545770ef8c74551b2f3b86298a673954e3e0f16
Andre Soares <andregsilv@gmail.com>
2021-08-04 23:00:34 -0300

    appl/ create transaction

--- application/application.go
+++ application/application.go
@@ -17,29 +17,43 @@ func New(
 	}
 }
 
-func (a _Application) CreateAccount(req CreateAccountRequest) (CreateAccountResponse, error) {
+func (a _Application) CreateAccount(req CreateAccountRequest) (resp CreateAccountResponse, err error) {
 	account := domain.NewAccount(
 		domain.DocumentNumber(req.DocumentNumber),
 	)
-	err := account.Validate()
+	err = account.Validate()
 	if err != nil {
-		return CreateAccountResponse{}, err
+		return
 	}
 
 	id, err := a.accountRepo.Save(account)
 	if err != nil {
-		return CreateAccountResponse{}, err
+		return
 	}
-
-	return CreateAccountResponse{
-		AccountID: id,
-	}, nil
+	resp.AccountID = id
+	return
 }
 
-func (a _Application) GetAccount(GetAccountRequest) (GetAccountResponse, error) {
+func (a _Application) GetAccount(req GetAccountRequest) (GetAccountResponse, error) {
 	return GetAccountResponse{}, nil
 }
 
-func (a _Application) CreateTransaction(CreateTransactionRequest) (CreateTransactionResponse, error) {
-	return CreateTransactionResponse{}, nil
+func (a _Application) CreateTransaction(req CreateTransactionRequest) (resp CreateTransactionResponse, err error) {
+	transaction := domain.NewTransaction(
+		req.AccountID,
+		domain.OperationType(req.OperationTypeID),
+		req.Amount,
+	)
+	err = transaction.Validate()
+	if err != nil {
+		return
+	}
+
+	id, err := a.transactionRepo.Save(transaction)
+	if err != nil {
+		return
+	}
+
+	resp.TransactionID = id
+	return
 }
