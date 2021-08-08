ceae016137a786f9b082f91dccd865a0ee444073
Andre Soares <andregsilv@gmail.com>
2021-08-08 09:57:41 -0300

    validate length of document number

--- domain/document_number.go
+++ domain/document_number.go
@@ -11,12 +11,16 @@ func (dn DocumentNumber) Validate() ValidationResult {
 		return InvalidResult("empty document number")
 	}
 
+	if len(dn) > 18 {
+		return InvalidResult("document number is too lengthy")
+	}
+
 	ok, err := regexp.MatchString(`^[0-9/.\-]+$`, string(dn))
 	if err != nil {
 		return ValidationErrorResult(err)
 	}
 	if !ok {
-		return InvalidResult("invalid characters on document number")
+		return InvalidResult("document number contains invalid characters")
 	}
 
 	return ValidResult()
