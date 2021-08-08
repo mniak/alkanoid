a205f2dc6c4e107be00e12ad2df6ab7ea24fc88e
Andre Soares <andregsilv@gmail.com>
2021-08-07 21:27:49 -0300

    test mapper

--- app/mapper_test.go
+++ app/mapper_test.go
@@ -2,6 +2,7 @@ package app_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/mniak/Alkanoid/app"
@@ -26,6 +27,19 @@ func TestGetAccountResponseFromAccount(t *testing.T) {
 	sut := app.NewMapper()
 	resp := sut.GetAccountResponseFromAccount(acc)
 
-	assert.Equal(t, resp.AccountID, acc.ID)
-	assert.Equal(t, resp.DocumentNumber, acc.DocumentNumber.String())
+	assert.Equal(t, acc.ID, resp.AccountID)
+	assert.Equal(t, acc.DocumentNumber.String(), resp.DocumentNumber)
+}
+
+func TestTransactionFromCreateTransactionRequest(t *testing.T) {
+	var req app.CreateTransactionRequest
+	gofakeit.Struct(&req)
+
+	sut := app.NewMapper()
+	tran := sut.TransactionFromCreateTransactionRequest(req)
+
+	assert.Equal(t, tran.AccountID, req.AccountID)
+	assert.Equal(t, tran.OperationType.ID(), req.OperationTypeID)
+	assert.Equal(t, tran.Amount, req.Amount)
+	assert.InDelta(t, time.Now().UnixNano(), tran.EventDate.UnixNano(), float64((100 * time.Millisecond).Nanoseconds()))
 }
